9bf4de3c3207947a55cc67764cc5a6a63911c467
Josh Campbell <josh@userdel.com>
2018-12-06 14:30:11 -0500

    finishes os.walk mocking

--- tests/test_whodis.py
+++ tests/test_whodis.py
@@ -1,39 +1,40 @@
 from os import getcwd
-from unittest import mock
+from unittest.mock import patch
 import pytest
 from whodis import WhoDis
 
 
-def test_determine_language_with_valid_path():
+@patch('os.walk')
+def test_determine_language_with_valid_path(mockwalk):
     """ Determine the language using a valid path with files in it """
-    with mock.patch('os.walk') as mockwalk:
-        mockwalk.return_value = [
-            ('/foo', ('bar',), ('README.md','requirements.txt')),
-            ('/foo/bar', (), ('__init__.py', 'main.py',)),
-        ]
-        blah = WhoDis()
-        blah.determine_language("/") #  Use / for the path since it's mocked.
+    mockwalk.return_value = [
+        ('/foo', ('bar',), ('README.md','requirements.txt')),
+        ('/foo/bar', (), ('__init__.py', 'main.py',)),
+    ]
+    blah = WhoDis()
+    blah.determine_language("/fake_path_yo")
 
-        # Tests for self.files.
-        assert type(blah.files) is list
-        assert len(blah.files) > 0
+    # Tests for self.files.
+    assert type(blah.files) is list
+    assert len(blah.files) > 0
 
-        # Tests for self.files_by_language.
-        assert type(blah.files_by_language) is dict
-        assert len(blah.files_by_language.keys()) > 0
-        assert "py" in blah.files_by_language.keys()
-        assert len(blah.files_by_language['py']) > 0
+    # Tests for self.files_by_language.
+    assert type(blah.files_by_language) is dict
+    assert len(blah.files_by_language.keys()) == 2
+    assert "py" in blah.files_by_language.keys()
+    assert "md" in blah.files_by_language.keys()
+    assert len(blah.files_by_language['py']) == 2
 
-        # Tests for self.language.
-        assert type(blah.language) is str
-        assert blah.language != ""
-        assert blah.language == 'py'
+    # Tests for self.language.
+    assert type(blah.language) is str
+    assert blah.language != ""
+    assert blah.language == 'py'
 
-        # Tests for self.all_languages.
-        assert type(blah.all_languages) is list
-        assert blah.all_languages != []
-        assert "py" in blah.all_languages
-        assert "md" in blah.all_languages
+    # Tests for self.all_languages.
+    assert type(blah.all_languages) is list
+    assert blah.all_languages != []
+    assert "py" in blah.all_languages
+    assert "md" in blah.all_languages
 
 
 def test_determine_language_with_invalid_path_raises_exception():
